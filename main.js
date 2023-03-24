79b839d9b39dab76c9df6f5f7c2eeeb76da5d1c5
PeytenBevill <redsage00@gmail.com>
2023-03-23 19:43:02 -0500

    Resolved instructed functions

--- main.js
+++ main.js
@@ -16,11 +16,28 @@ const displayDate = () => {
 
 // Write a JavaScript program to convert a number to a string.
 
+function numToString(num) {
+  // console.log(typeof num)
+  return num.toString()
+}
+
+const runNumToString = () => console.log(numToString(8))
+
+// const stringAsNumber = numToString(42)
+// console.log(typeof stringAsNumber)
+
+
+
 
 
 // Write a JavaScript program to convert a string to the number.
 
+const convertNum = (string) => Number(string)
 
+const runConvertNum = () => console.log(convertNum("9"))
+
+
+// console.log(convertNum('5'))
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
   // * Boolean
@@ -29,22 +46,83 @@ const displayDate = () => {
   // * Number
   // * NaN
   // * String
+
+  const whatTypeOf = (data) => {
+    return typeof data
+  }
+
+  const runWhatTypeOf = () => {
+    console.log(whatTypeOf(false))
+  }
   
+  // console.log(whatTypeOf(1))
+  // console.log(whatTypeOf('hat'))
+  // console.log(whatTypeOf(null))
+  // console.log(whatTypeOf(false))
+
+
+
+
 
   
 // Write a JavaScript program that adds 2 numbers together.
 
+const numAdd = (x, y) => {
+  return x + y
+}
 
+const runNumAdd = () => {
+  console.log(numAdd(128, 345))
+}
+
+// console.log(numAdd(12, 24))
 
 // Write a JavaScript program that runs only when 2 things are true.
 
+const bothTrue = (x, y) => {
+  if (x && y) {
+    return true
+  } else {
+    return undefined
+  }
+}
 
+const runBothTrue = () => {
+  console.log(bothTrue(5, 7))
+}
+
+// console.log(bothTrue(3, 'hi'))
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
 
+const eitherTrue = (x, y) => {
+  if (x || y) {
+    return "One is true"
+  } else {
+    return undefined
+  }
+}
 
+const runEitherTrue = () => {
+  console.log(eitherTrue(3, -0))
+}
 
+// console.log(eitherTrue('True', -0))
 // Write a JavaScript program that runs when both things are not true.  
+
+const notTrue = (x, y) => {
+  if (!x && !y) {
+    return "Both are not true"
+  } else {
+    return "True :("
+  }
+}
+
+const runNotTrue = () => {
+  console.log(notTrue('', -0))
+}
+
+// console.log(notTrue('', -0))
 
 // ***************************
 //         PART TWO
